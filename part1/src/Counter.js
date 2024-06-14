e95632b432cb7eab986ec8a1bfa27928c2dae734
Jisna Sreejesh <jisi.sree@gmail.com>
2024-06-14 12:53:54 -0400

    Tried state as an object

--- part1/src/Counter.js
+++ part1/src/Counter.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+/*import {useState} from 'react';
 const Counter =() =>{
 
     const [counter,setCounter] = useState(0);
@@ -23,5 +23,35 @@ const Counter =() =>{
             <button onClick={decreaseCounter} text="minus"/>
         </div>
     )
+}
+export default Counter;
+*/
+import {useState} from 'react';
+const Counter = () =>{
+    const [clicks,setClicks] = useState({
+        left: 0,
+        right: 0
+    });
+const rightClickHandler = () =>{
+    setClicks({
+        left: clicks.left,
+        right: clicks.right +1
+    })
+}
+    const leftClickHandler = () =>{
+        setClicks({
+            left: clicks.left +1,
+            right: clicks.right
+        })
+    }
+    return(
+        <div>
+            <h1>{clicks.left}</h1>
+            <button onClick={leftClickHandler}>Left</button>
+            <h1>{clicks.right}</h1>
+            <button onClick={rightClickHandler}>Right</button>
+        </div>
+    );
+
 }
 export default Counter;
